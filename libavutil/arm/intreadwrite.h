20f157d196e7f1dba54ff86060cba6bae576991b
Chen Minqiang <ptpt52@gmail.com>
2021-05-25 13:15:21 +0800

    avutil/attributes: fix build strh/ldrh

--- libavutil/arm/intreadwrite.h
+++ libavutil/arm/intreadwrite.h
@@ -31,11 +31,11 @@ static av_always_inline unsigned AV_RN16(const void *p)
     const uint8_t *q = p;
     unsigned v;
 #if AV_GCC_VERSION_AT_MOST(4,5)
-    __asm__ ("ldrh %0, %1" : "=r"(v) : "m"(*(const uint16_t *)q));
+    __asm__ ("ldrh %0, %w1" : "=r"(v) : "m"(*(const uint16_t *)q));
 #elif defined __thumb__
-    __asm__ ("ldrh %0, %1" : "=r"(v) : "m"(q[0]), "m"(q[1]));
+    __asm__ ("ldrh %0, %w1" : "=r"(v) : "m"(q[0]), "m"(q[1]));
 #else
-    __asm__ ("ldrh %0, %1" : "=r"(v) : "Uq"(q[0]), "m"(q[1]));
+    __asm__ ("ldrh %0, %w1" : "=r"(v) : "Uq"(q[0]), "m"(q[1]));
 #endif
     return v;
 }
@@ -43,7 +43,7 @@ static av_always_inline unsigned AV_RN16(const void *p)
 #define AV_WN16 AV_WN16
 static av_always_inline void AV_WN16(void *p, uint16_t v)
 {
-    __asm__ ("strh %1, %0" : "=m"(*(uint16_t *)p) : "r"(v));
+    __asm__ ("strh %w1, %0" : "=m"(*(uint16_t *)p) : "r"(v));
 }
 
 #define AV_RN32 AV_RN32
